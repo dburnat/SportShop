56767c7432f7a51818a1c0363ecdb29c37743df6
dburnat <d.burnat97@gmail.com>
2019-11-08 10:14:51 +0100

    Added methods to Save and Delete product

--- SportShop/Models/EFProductRepository.cs
+++ SportShop/Models/EFProductRepository.cs
@@ -14,5 +14,36 @@ namespace SportShop.Models
             this.dbContext = dbContext;
         }
         public IQueryable<Product> Products =>  dbContext.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                dbContext.Products.Add(product);
+            }
+            else
+            {
+                Product productToEdit = dbContext.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (productToEdit != null)
+                {
+                    productToEdit.Name = product.Name;
+                    productToEdit.Price = product.Price;
+                    productToEdit.Description = product.Description;
+                    productToEdit.Rating = product.Rating;
+                }
+            }
+            dbContext.SaveChanges();
+        }
+
+        public Product DeleteProduct(int id)
+        {
+            Product productToDelete = dbContext.Products.FirstOrDefault(p => p.ProductID == id);
+            if (productToDelete != null)
+            {
+                dbContext.Products.Remove(productToDelete);
+                dbContext.SaveChanges();
+            }
+            return productToDelete;
+        }
     }
 }
