3d0c059e0dab93230f1cfb4a90f176ba6766b0ad
dburnat <d.burnat97@gmail.com>
2019-11-08 11:31:23 +0100

    Product validation rules

--- SportShop/Models/Product.cs
+++ SportShop/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace SportShop.Models
     public class Product 
     {
         public int ProductID { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
         public string Category { get; set; }
+        [RegularExpression(@"\d")]
+        [MaxLength(1)]
+        [Required]
         public int Rating { get; set; }
         public int? ManufacturerId { get; set; }
         public Manufacturer Manufacturer { get; set; }
