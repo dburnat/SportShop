e908eb0b625a6f3246554c71f11bc9bda8c1c03a
dburnat <d.burnat97@gmail.com>
2019-11-21 20:35:08 +0100

    Added EFCategoryRepository

---
+++ SportShop/Models/EFCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportShop.Models
+{
+    public class EFCategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EFCategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IQueryable<Category> Categories => dbContext.Categories;
+    }
+}
