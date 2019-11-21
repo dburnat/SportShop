667bbb743b4d590ca29b7bf9dbcf37910f534ffb
dburnat <d.burnat97@gmail.com>
2019-11-21 19:38:05 +0100

    Added ICategoryRepository

---
+++ SportShop/Models/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IQueryable<Category> Categories { get; }
+    }
+}
