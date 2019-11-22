c3e2c83c5d2e16ba0c9718efe5a78840994ef365
dburnat <d.burnat97@gmail.com>
2019-11-22 10:17:28 +0100

    Admin controller is now authorized

--- SportShop/Controllers/AdminController.cs
+++ SportShop/Controllers/AdminController.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SportShop.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private readonly IProductRepository _productRepository;
