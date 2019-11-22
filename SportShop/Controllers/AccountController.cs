c5aba8c3d582a13ae98ac2b71a15ac551a2c6a8d
dburnat <d.burnat97@gmail.com>
2019-11-22 11:02:00 +0100

    Added AccountController and viewmodel for logging in

---
+++ SportShop/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportShop.Views;
+using SportShop.Models;
+
+namespace SportShop.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> user, SignInManager<IdentityUser> signIn)
+        {
+            userManager = user;
+            signInManager = signIn;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    if ((await signInManager.PasswordSignInAsync(user,loginModel.Password,false,false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Nieprawidłowe dane logowania");
+            return View(loginModel);
+        }
+
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
