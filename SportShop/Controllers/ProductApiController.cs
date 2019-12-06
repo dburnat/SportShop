4d00d18419549a88a8357023d7b873c6acf62b38
dburnat <d.burnat97@gmail.com>
2019-12-06 11:25:59 +0100

    Added swagger and ApiController for Product

---
+++ SportShop/Controllers/ProductApiController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportShop.Models;
+
+namespace SportShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public ProductApiController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        /// <summary>
+        /// Returns list of all products currently stored in database
+        /// </summary>
+        /// <returns>List of all products</returns>
+        [HttpGet]
+        public IEnumerable<Product> Get() => _productRepository.Products;
+        /// <summary>
+        /// Returns product with specific id
+        /// </summary>
+        /// <param name="id">Integer</param>
+        /// <returns>Product</returns>
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Product product = _productRepository.Products.FirstOrDefault(p => p.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+        /// <summary>
+        /// Creates a new product
+        /// </summary>
+        /// <param name="product">Product object</param>
+        /// <returns>Product that was created</returns>
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _productRepository.SaveProduct(product);
+
+            return CreatedAtAction(nameof(GetById), new { id = product.ProductID }, product);
+        }
+        /// <summary>
+        /// Edits a product with specific id
+        /// </summary>
+        /// <param name="id">Integer</param>
+        /// <returns>Product that was edited</returns>
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id)
+        {
+            Product product = _productRepository.Products.FirstOrDefault(p => p.ProductID == id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _productRepository.SaveProduct(product);
+            }
+            catch (DbUpdateException)
+            {
+                if (_productRepository.Products.FirstOrDefault(p => p.ProductID == id) !=null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtAction(nameof(GetById), new { id = product.ProductID }, product);
+        }
+        /// <summary>
+        /// Deletes product with specific id
+        /// </summary>
+        /// <param name="id">Integer</param>
+        /// <returns>StatusCode200</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var product = _productRepository.Products.FirstOrDefault(p => p.ProductID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.DeleteProduct(id);
+
+            return Ok(product);
+        }
+       
+    }
+}
--- SportShop/Startup.cs
+++ SportShop/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportShop.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace SportShop
 {
@@ -43,11 +46,22 @@ namespace SportShop
                 options.Password.RequireNonAlphanumeric = false;
 
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SportShopAPI", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+
 
             app.UseDeveloperExceptionPage();
 
@@ -64,6 +78,11 @@ namespace SportShop
 
             );
             SeedData.EnsurePopulated(app);
+            app.UseSwagger();
+            app.UseSwaggerUI( c=> 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SportShopAPIv1");
+            });
 
 
         }
