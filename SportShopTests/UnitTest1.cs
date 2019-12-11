2e848d261fc7e5a371270b516ad381a32b04304c
dburnat <d.burnat97@gmail.com>
2019-12-11 22:51:43 +0100

    Implemented inlinedata to ReturningProductById test

--- SportShopTests/UnitTest1.cs
+++ SportShopTests/UnitTest1.cs
@@ -61,25 +61,25 @@ namespace SportShopTests
             Assert.Equal("first2",array[1].Name);
         }
 
-        [Fact]
-        public void ReturningProductById()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void ReturningProductById(int value)
         {
             var mock = new Mock<IProductRepository>();
             mock.Setup(p => p.Products).Returns(new[]
             {
                 new Product {ProductID = 1, Name = "first", CategoryID = 1},
-                new Product {ProductID = 1, Name = "first2", CategoryID = 1},
                 new Product {ProductID = 2, Name = "second", CategoryID = 2},
                 new Product {ProductID = 3, Name = "third", CategoryID = 3},
             }.AsQueryable);
 
             var controller = new ProductController(mock.Object);
-            var result = controller.ProductId(1);
+            var result = controller.ProductId(value);
             var model = (IEnumerable<Product>) (result).Model;
-            var array = model.ToArray();
-            Assert.Equal(2, model.Count());
-            Assert.Equal("first", array[0].Name);
-            Assert.Equal("first2",array[1].Name);
+            Assert.Equal(1, model.Count());
+            
         }
     }
 }
