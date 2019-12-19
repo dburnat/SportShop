b04c881e4bc636cd7f59f9bfd71eb9bb0ac72241
dburnat <d.burnat97@gmail.com>
2019-12-19 18:58:30 +0100

    Split tests by controllers

---
+++ SportShopTests/AdminControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportShop.Controllers;
+using SportShop.Models;
+using Xunit;
+
+namespace SportShopTests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void IndexReturnsAllProducts_ShouldReturnCollectionContainingEveryProduct()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "first"},
+                new Product {ProductID = 2, Name = "second"},
+                new Product {ProductID = 3, Name = "third"},
+                new Product {ProductID = 4, Name = "fourth", CategoryID = 15},
+            }.AsQueryable);
+
+
+            var controller = new AdminController(mock.Object);
+            
+            var result = controller.Index();
+
+            var model = (IEnumerable<Product>)(result).Model;
+            var array = model.ToArray();
+
+            Assert.Equal(4, model.Count());
+            Assert.Equal("first",array[0].Name);
+            Assert.Equal("second",array[1].Name);
+            Assert.Equal("third",array[2].Name);
+            Assert.Equal("fourth", array[3].Name);
+        }
+        [Fact]
+        public void DeletingProduct_ShouldBeCalledOnlyOnce()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "first", CategoryID = 1},
+                new Product {ProductID = 2, Name = "second", CategoryID = 2},
+                new Product {ProductID = 3, Name = "third", CategoryID = 3},
+            }.AsQueryable);
+
+            var controller = new AdminController(mock.Object);
+            controller.Delete(mock.Object.Products.ElementAt(0));
+            mock.Verify(m => m.DeleteProduct(mock.Object.Products.ElementAt(0).ProductID), Times.Once());
+        }
+
+        [Fact]
+        public void SavingProductWithModelError_ShouldReturnEditViewResult()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "first", CategoryID = 1},
+                new Product {ProductID = 2, Name = "second", CategoryID = 2},
+                new Product {ProductID = 3, Name = "third", CategoryID = 3},
+            }.AsQueryable);
+            var product = new Product
+            {
+                ProductID = 5, Name = "fifth"
+            };
+
+            var controller = new AdminController(mock.Object);
+            controller.ModelState.AddModelError("error", "errormessage");
+
+            var result = controller.Save(product) as ViewResult;
+            Assert.Equal("Edit", result.ViewName);
+        }
+    }
+}
--- SportShopTests/ProductControllerTests.cs
+++ SportShopTests/ProductControllerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,37 +12,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SportShopTests
 {
-    public class UnitTest1
+    public class ProductControllerTests
     {
-        
-
-        [Fact]
-        public void IndexReturnsAllProducts()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void ReturningProductById_ShouldReturnSingleProduct(int value)
         {
             var mock = new Mock<IProductRepository>();
             mock.Setup(p => p.Products).Returns(new[]
             {
-                new Product {ProductID = 1, Name = "first"},
-                new Product {ProductID = 2, Name = "second"},
-                new Product {ProductID = 3, Name = "third"},
+                new Product {ProductID = 1, Name = "first", CategoryID = 1},
+                new Product {ProductID = 2, Name = "second", CategoryID = 2},
+                new Product {ProductID = 3, Name = "third", CategoryID = 3},
             }.AsQueryable);
 
-
-            var controller = new AdminController(mock.Object);
-            
-            var result = controller.Index();
-
-            var model = (IEnumerable<Product>)(result).Model;
-            var array = model.ToArray();
-
-            Assert.Equal(3, model.Count());
-            Assert.Equal("first",array[0].Name);
-            Assert.Equal("second",array[1].Name);
-            Assert.Equal("third",array[2].Name);
+            var controller = new ProductController(mock.Object);
+            var result = controller.ProductId(value);
+            var model = (IEnumerable<Product>) result.Model;
+            Assert.Single(model);
         }
-
+        
         [Fact]
-        public void ReturningProductByCategory()
+        public void ReturningProductByCategory_ShouldReturn2()
         {
             var mock = new Mock<IProductRepository>();
             mock.Setup(p => p.Products).Returns(new[]
@@ -61,11 +55,26 @@ namespace SportShopTests
             Assert.Equal("first2",array[1].Name);
         }
 
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        public void ReturningProductById(int value)
+        [Fact]
+        public void ReturningProductByCategory_ReturnsEmptyCollection()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(p => p.Products).Returns(new[]
+            {
+                new Product {ProductID = 1, Name = "first", CategoryID = 1},
+                new Product {ProductID = 1, Name = "first2", CategoryID = 1},
+                new Product {ProductID = 2, Name = "second", CategoryID = 2},
+                new Product {ProductID = 3, Name = "third", CategoryID = 3},
+            }.AsQueryable);
+
+            var controller = new ProductController(mock.Object);
+            var result = controller.List(4);
+            var model = (IEnumerable<Product>) result.Model;
+            Assert.Empty(model);
+        }
+
+        [Fact]
+        public void ReturningProductById_ShouldReturnEmptyCollection()
         {
             var mock = new Mock<IProductRepository>();
             mock.Setup(p => p.Products).Returns(new[]
@@ -76,10 +85,9 @@ namespace SportShopTests
             }.AsQueryable);
 
             var controller = new ProductController(mock.Object);
-            var result = controller.ProductId(value);
-            var model = (IEnumerable<Product>) (result).Model;
-            Assert.Equal(1, model.Count());
-            
+            var result = controller.ProductId(4);
+            var model = (IEnumerable<Product>) result.Model;
+            Assert.Empty(model);
         }
     }
 }
